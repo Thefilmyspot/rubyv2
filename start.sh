175ab7edf7ff676df0c72870b0ebba783e99d1c4
ABHINAND A S <abhinandunnikuttan04@gmail.com>
2022-08-12 15:09:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/abhinand2608/anadearmas
+  git clone https://github.com/abhinand2608/thefilmyspotbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /abhinand2608/anadearmas
+  git clone $UPSTREAM_REPO /EvaMaria
 fi
-cd /anadearmas
+cd /EvaMaria
 pip3 install -U -r requirements.txt
-echo "Starting anadearmas ...."
+echo "Starting Bot...."
 python3 bot.py
